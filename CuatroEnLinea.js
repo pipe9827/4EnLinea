46c360533b55912a756df805f117d0d4247e1ef9
Andres Felipe Sabogal Rada <afsabogalr@gmail.com>
2025-03-04 22:26:28 -0500

    Actualización a actions/upload-artifact 4

--- CuatroEnLinea.js
+++ CuatroEnLinea.js
@@ -4,14 +4,7 @@ var CuatroEnLinea = /** @class */ (function () {
         if (cols === void 0) { cols = 7; }
         this.rows = rows;
         this.cols = cols;
-        this.board = [];
-        // Crear la matriz manualmente en lugar de usar Array.from()
-        for (var i = 0; i < this.rows; i++) {
-            this.board[i] = [];
-            for (var j = 0; j < this.cols; j++) {
-                this.board[i][j] = 0;
-            }
-        }
+        this.board = Array.from({ length: rows }, function () { return Array(cols).fill(0); });
         this.currentPlayer = 1;
         this.initBoard();
     }
@@ -19,9 +12,7 @@ var CuatroEnLinea = /** @class */ (function () {
         var _this = this;
         var boardElement = document.getElementById("board");
         boardElement.innerHTML = "";
-        boardElement.style.display = "grid";
-        boardElement.style.gridTemplateColumns = "repeat(".concat(this.cols, ", 60px)"); // Asegurar que el tablero tenga el formato correcto
-        boardElement.style.gridGap = "5px";
+        boardElement.classList.add("board");
         for (var row = 0; row < this.rows; row++) {
             var _loop_1 = function (col) {
                 var cell = document.createElement("div");
@@ -37,12 +28,16 @@ var CuatroEnLinea = /** @class */ (function () {
         }
     };
     CuatroEnLinea.prototype.placeToken = function (col) {
+        var _this = this;
         for (var row = this.rows - 1; row >= 0; row--) {
             if (this.board[row][col] === 0) {
                 this.board[row][col] = this.currentPlayer;
-                this.updateBoard();
+                this.animateToken(row, col);
                 if (this.checkWinner(row, col)) {
-                    alert("\u00A1Jugador ".concat(this.currentPlayer, " ha ganado!"));
+                    setTimeout(function () {
+                        alert("\u00A1Jugador ".concat(_this.currentPlayer, " ha ganado!"));
+                        _this.highlightWinningTokens();
+                    }, 500);
                     return;
                 }
                 this.currentPlayer = this.currentPlayer === 1 ? 2 : 1;
@@ -51,24 +46,41 @@ var CuatroEnLinea = /** @class */ (function () {
             }
         }
     };
-    CuatroEnLinea.prototype.updateBoard = function () {
-        var _this = this;
-        var cells = document.querySelectorAll(".cell");
-        cells.forEach(function (cell) {
-            var row = parseInt(cell.dataset.row);
-            var col = parseInt(cell.dataset.column);
-            var player1Color = document.getElementById("player1-color").value;
-            var player2Color = document.getElementById("player2-color").value;
-            var color = _this.board[row][col] === 1 ? player1Color :
-                _this.board[row][col] === 2 ? player2Color : "white";
-            cell.style.backgroundColor = color;
-        });
+    CuatroEnLinea.prototype.animateToken = function (row, col) {
+        var player1Color = document.getElementById("player1-color").value;
+        var player2Color = document.getElementById("player2-color").value;
+        var color = this.currentPlayer === 1 ? player1Color : player2Color;
+        var tempRow = 0;
+        var interval = setInterval(function () {
+            if (tempRow > 0) {
+                var prevCell = document.querySelector(".cell[data-row='".concat(tempRow - 1, "'][data-column='").concat(col, "']"));
+                prevCell.style.backgroundColor = "white";
+            }
+            var currentCell = document.querySelector(".cell[data-row='".concat(tempRow, "'][data-column='").concat(col, "']"));
+            currentCell.style.backgroundColor = color;
+            currentCell.classList.add("falling");
+            if (tempRow === row) {
+                clearInterval(interval);
+                currentCell.classList.remove("falling");
+                currentCell.classList.add("bounce");
+            }
+            else {
+                tempRow++;
+            }
+        }, 50);
     };
     CuatroEnLinea.prototype.checkWinner = function (row, col) {
         return this.checkDirection(row, col, 1, 0) || // Vertical
             this.checkDirection(row, col, 0, 1) || // Horizontal
             this.checkDirection(row, col, 1, 1) || // Diagonal /
             this.checkDirection(row, col, 1, -1); // Diagonal \
+    };
+    CuatroEnLinea.prototype.highlightWinningTokens = function () {
+        document.querySelectorAll(".cell").forEach(function (cell) {
+            if (getComputedStyle(cell).backgroundColor !== "rgb(255, 255, 255)") {
+                cell.classList.add("winning-cell");
+            }
+        });
     };
     CuatroEnLinea.prototype.checkDirection = function (row, col, rowDir, colDir) {
         var count = 1;
